7ffb5a4fb3068850e8d350d1b5c8b760f0efb6c1
RexShack <rexshackuk@gmail.com>
2025-12-11 17:53:16 +0000

    add install stuff

---
+++ installation/shared_items.lua
@@ -0,0 +1,4 @@
+    -----------------------------------------------
+    -- YOUR CUSTOM ITEMS
+    -----------------------------------------------
+    shovel = { name = 'shovel', label = 'Shovel', weight = 100, type = 'item', image = 'shovel.png', unique = false, useable = true, shouldClose = true, description = 'for digging up stuff' },
